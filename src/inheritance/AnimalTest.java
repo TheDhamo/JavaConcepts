6d013c34ab467437b142b7f65f58571651c15dde
Sumon <sdtechie22@gmail.com>
2019-06-11 20:05:38 +0530

    Added Inheritance

--- src/inheritance/AnimalTest.java
+++ src/inheritance/AnimalTest.java
@@ -7,19 +7,19 @@ public class AnimalTest {
 		Animal genericAnimal = new Animal("SpeciesX", 1, 1, 30);
 		genericAnimal.eat();
 		genericAnimal.move();
-		System.out.println("****************************************************************");
+		System.out.println("*****************************************************************");
 
 		Cat homeCat = new Cat("Suzy", 23, 2, 4, 1, 15);
 		homeCat.eat();
 		homeCat.move();
 		homeCat.run();
-		System.out.println("****************************************************************");
+		System.out.println("*****************************************************************");
 
 		Fish angelFish = new Fish("Angel", 2, 1, 2, 4);
 		angelFish.eat();
 		angelFish.move();
 		angelFish.swim();
-		System.out.println("****************************************************************");
+		System.out.println("*****************************************************************");
 	}
 
 }
