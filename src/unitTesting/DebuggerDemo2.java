fbc0ebf8db3f54bf827652e6ea6539a125df4bad
Sumon <sdtechie22@gmail.com>
2019-08-26 08:59:13 +0530

    Added Unit Testing using Junit

---
+++ src/unitTesting/DebuggerDemo2.java
@@ -0,0 +1,10 @@
+package eighteenthSection;
+
+public class DebuggerDemo2 {
+
+	public static void main(String[] args) {
+		String name = "Sam Dey";
+		System.out.println("Name is: " + name);
+	}
+
+}
