9434e381a687b67017e3c66d41ee70da60cd378b
Sumon <sdtechie22@gmail.com>
2020-02-11 21:03:17 +0530

    Added more info on variables and blocks

--- src/basics/JavaVariables.java
+++ src/basics/JavaVariables.java
@@ -1,4 +1,4 @@
-/*Java Variables*/
+/*Java Variables and Block*/
 package basics;
 
 public class JavaVariables {
@@ -8,10 +8,8 @@ public class JavaVariables {
 	 * 3 types - 1) Instance (Member) Variables 2) Static (Class) Variables 3)
 	 * Local Variables.
 	 */
-	private static int heightOfPerson = 173;
-	/* Here, "heightOfPerson" is a static (class) variable. */
-	private int weightOfPerson;
-	/* Here, "weightOfPerson" is an instance (member) variable. */
+	private static int heightOfPerson = 173; // static or class variable
+	private int weightOfPerson; // instance or member variable
 
 	public static void main(String[] args) {
 		JavaVariables obj1 = new JavaVariables();
@@ -21,14 +19,29 @@ public class JavaVariables {
 		JavaVariables obj2 = new JavaVariables();
 		obj2.setWeightOfPerson(192);
 		obj2.biometrics(false);
+		System.out.println("***************************************************************");
+		obj2.blockTest();
 	}
 
-	public void biometrics(boolean overThirty) {
-		int age; /* Here, "age" and "overThirty" are local variables. */
+	private void biometrics(boolean overThirty) {
+		int age;
+		// "age" and "overThirty" are local variables
 		age = overThirty ? 31 : 29;
 		System.out.println("Height of the person is: " + heightOfPerson + " cm.(static)");
 		System.out.println("Weight of the person is: " + weightOfPerson + " lbs.(instance)");
 		System.out.println("Age of the person is: " + age + " years.(local)");
+	}
+
+	// lifetime and visibility of variables within blocks
+	private void blockTest() {
+		int a = 24;
+		if (a > 20) {
+			int b = a / 2;
+			System.out.println("Value of b inside if-block: " + b);
+		}
+		// System.out.println("Value of b outside if-block: " + b);
+		// The above line will not compile since the lifetime and scope of "b"
+		// ends with the end of the if-block
 	}
 
 	public int getWeightOfPerson() {
