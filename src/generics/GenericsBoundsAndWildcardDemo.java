59d04cdb193408e45fbc7cf7bfc49ab44c26952f
Sumon <sdtechie22@gmail.com>
2020-03-21 20:21:33 +0530

    Added Generics Wildcard

--- src/generics/GenericsBoundsAndWildcardDemo.java
+++ src/generics/GenericsBoundsAndWildcardDemo.java
@@ -1,6 +1,6 @@
 package generics;
 
-public class GenericsBoundsDemo {
+public class GenericsBoundsAndWildcardDemo {
 
 	public static void main(String[] args) {
 		// Integer is a subclass of "Number"
@@ -13,10 +13,31 @@ public class GenericsBoundsDemo {
 		Stats<Double> dOb = new Stats<Double>(iDums);
 		System.out.println("Average is: " + dOb.average());
 
+		// Float is also a subclass of "Number"
+		Float[] iFums = { 1.0F, 2.0F, 3.0F, 4.0F, 5.0F };
+		Stats<Float> fOb = new Stats<Float>(iFums);
+		System.out.println("Average is: " + fOb.average());
+
 		// String is not a subclass of "Number" hence the below will throw Compile Error
 		// String[] iSums = { "1", "2", "3", "4", "5" };
 		// Stats<String> sOb = new Stats<String>(iSums);
 		// System.out.println("Average is: " + sOb.average());
+
+		System.out.println("*******************************************************");
+
+		System.out.print("Averages of iOb and dOb ");
+		if (iOb.sameAverage(dOb)) {
+			System.out.println("are the same");
+		} else {
+			System.out.println("differ");
+		}
+
+		System.out.print("Averages of iOb and fOb ");
+		if (iOb.sameAverage(fOb)) {
+			System.out.println("are the same");
+		} else {
+			System.out.println("differ");
+		}
 
 	}
 
@@ -36,5 +57,13 @@ class Stats<T extends Number> {
 			sum += ob[i].doubleValue();
 		}
 		return sum / ob.length;
+	}
+
+	// check whether average is same or not
+	public boolean sameAverage(Stats<?> ob) {
+		if (this.average() == ob.average()) {
+			return true;
+		}
+		return false;
 	}
 }
