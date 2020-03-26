42e08362da758a9bbf04cb9bf59c040dc2fbcc73
Sumon <sdtechie22@gmail.com>
2020-03-27 00:29:23 +0530

    Added methods used for String modification

---
+++ src/stringHandling/StringModification.java
@@ -0,0 +1,28 @@
+package stringHandling;
+
+public class StringModification {
+
+	public static void main(String[] args) {
+		String str1 = "Hello Java World";
+
+		// substring()
+		System.out.println(str1.substring(2));
+		System.out.println(str1.substring(2, 10));
+		System.out.println("**************************************************");
+
+		// concat()
+		System.out.println(str1.concat(" ...this is added after the statement."));
+		System.out.println("**************************************************");
+
+		// replace()
+		System.out.println(str1.replace('o', '\u00A9'));
+		System.out.println(str1.replace("Java", "Python"));
+		System.out.println("**************************************************");
+
+		// trim() -> to remove the leading and trailing whitespaces
+		String str2 = "   Hello World Java         \t \r \n     ";
+		System.out.println(str2.trim());
+		System.out.println("**************************************************");
+	}
+
+}
