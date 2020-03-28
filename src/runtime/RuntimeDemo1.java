279b5633682c73bb2c13c67f919d75ab62e3cf0a
Sumon <sdtechie22@gmail.com>
2020-03-29 00:19:03 +0530

    Added info on Memory Management

--- src/runtime/RuntimeDemo1.java
+++ src/runtime/RuntimeDemo1.java
@@ -3,7 +3,18 @@ package runtime;
 public class RuntimeDemo1 {
 
 	public static void main(String[] args) {
+		/*
+		 * The "Runtime" class encapsulates the Java Runtime Environment and helps to
+		 * control the state and behavior of the JVM
+		 */
+		/*
+		 * We cannot create an object of the Runtime class but can get a reference to
+		 * the current "Runtime" object by calling the static method getRuntime()
+		 */
 		Runtime r = Runtime.getRuntime();
+
+		// The "Process" is an abstract class that encapsulates a process, i.e. an
+		// executing program
 		Process p = null;
 		try {
 			p = r.exec("notepad");
